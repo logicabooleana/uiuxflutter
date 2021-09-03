ce85237989f39a7832f8a75bd095806a038a7985
Lógica Booleana <logica.booleana.2019@gmail.com>
2021-09-03 10:56:31 -0300

    dele directory test 'app'

--- lib/app/modules/home/bindings/login_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:desingapp/app/modules/home/controllers/login_controller.dart';
-
-class LoginBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.put<LoginController>(LoginController());
-  }
-}
--- lib/app/modules/home/bindings/welcome_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:desingapp/app/modules/home/controllers/welcome_controller.dart';
-
-class WelcomeBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.put<WelcomeController>(WelcomeController());
-  }
-}
